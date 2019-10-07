3ca92f6b03382ee6c35bfd587482a2ad2b48ca9c
Vince <vincent_field@foxmail.com>
2019-10-08 01:38:36 +0800

    add number 11

---
+++ #11/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int max = (j - i) * (height[i] < height[j] ? height[i] : height[j]);
+        
+        while (i < j - 1) {
+            
+            if (height[i] < height[j]) {
+                while (height[i++] >= height[i] && i < j);
+            } else {
+                while (height[j--] >= height[j] && i < j);
+            }
+            
+            int temp;
+            if ((temp = (j - i) * (height[i] < height[j] ? height[i] : height[j])) > max) {
+                max = temp;
+            }
+        }
+        return max;
+    }
+    
+}
